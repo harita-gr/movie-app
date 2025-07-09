db887c90c911c2acbeac3b5ef3e4a87530ac865d
harita-gr <haritagr19@gmail.com>
2025-07-10 01:01:14 +0900

    initial commit

---
+++ src/appwrite.js
@@ -0,0 +1,62 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+
+const client = new Client()
+  .setEndpoint(ENDPOINT) // Your Appwrite Endpoint
+  .setProject(PROJECT_ID); // Your project ID
+
+const database = new Databases(client);
+
+// export const testAppwriteConnection = async () => {
+//   try {
+//     const result = await account.get(); // Will fail if not logged in
+//     console.log("Appwrite connected! User data:", result);
+//   } catch (err) {
+//     console.log("Appwrite SDK connection working. No user logged in.");
+//   }
+// };
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  // 1. Use Appwrite SDK to check if the search term exists in the database
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
+
+    // 2. If it does, update the count
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+      // 3. If it doesn't, create a new document with the search term and count as 1
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+  }
+};
